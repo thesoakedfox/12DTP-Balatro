e88768d3fd977240017539c22aba7a5411673a68
thesoakedfox <yushuangyuan18@gmail.com>
2025-05-09 18:18:21 +1300

    added 404 page

--- app/app.py
+++ app/app.py
@@ -110,5 +110,10 @@ def jokers():
                           type_filter=type_filter,
                           activation_filter=activation_filter)
 
+# Custom 404 error handler
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
